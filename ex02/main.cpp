1ceb8f35176364dcf2edcb5efbd274a1854de82b
Lucas Sinke <lsinke@student.codam.nl>
2023-04-25 17:25:44 +0200

    Add the provided test to ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -13,6 +13,57 @@
 #include <iostream>
 #include "Array.hpp"
 
+#define MAX_VAL 750
+static int provided_main()
+{
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
+
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+	delete [] mirror;//
+	return 0;
+}
+
 static void testInt() {
 	Array<int> arr(100);
 	for (int i = 0; i < 100; i++)
@@ -55,4 +106,6 @@ int main() {
 	testInt();
 	testAssignCopy();
 	testOOB();
+
+	provided_main();
 }
